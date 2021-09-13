c57e54d8d05437adbac660fc840e8afd8b8d09d1
Howard Kwon <howie.ykwon70@gmail.com>
2021-09-13 02:40:54 -0400

    commit tree 9c635045e96badc07ef00d1e6e9cd28bcb15439a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,38 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
+  get '/' do
+    'Hello Baked Fans!'
+  end
+
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods' do
+    baked_goods = BakedGood.all
+    baked_goods.to_json
+  end
+
+
+  # *Can make more dynamic by making a self.by_price in BakedGood class
+  # def self.by_price
+  #   self.all.order(price: :desc)
+  #  end
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.all.order(price: :desc)
+    baked_goods.to_json
+  end 
+
+  get '/baked_goods/most_expensive' do
+    baked_goods = BakedGood.all.order(price: :desc).first
+    baked_goods.to_json
+  end 
 
 end
